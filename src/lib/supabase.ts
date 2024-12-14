78bb65940b08b28c1f8c9cd58ece986c045721d6
conex-ia <typebotconexi9@gmail.com>
2024-12-14 20:36:04 -0300

    Update supabase.ts
    Proteção das credenciais

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://tfmzozvazfbrapkzxrcz.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRmbXpvenZhemZicmFwa3p4cmN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjMzMjMwNTIsImV4cCI6MjAzODg5OTA1Mn0.IddIxLB1B4rg0A87lp4ZQzyu1mhTppo7SW_CjP3rwCw';
+const supabaseUrl = 'urlBase';
+const supabaseKey = 'AnonKey';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
